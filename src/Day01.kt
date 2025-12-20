2e7bde5239c197c79a627a8522c577c32a89ff84
Matthias Wiench <matthias.wiench@sap.com>
2025-12-20 14:27:51 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,54 @@
+import kotlin.math.absoluteValue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var dial = List(100, {i -> (i * 1)})
+        var currentIndexDial = 50
+        var zeroCounter = 0
+        for (line in input) {
+            val distance = line.split("R", "L").last().toInt() % 100;
+            val direction = line.first().toString();
+            if (direction == "R") {
+                currentIndexDial = currentIndexDial + distance
+                if (currentIndexDial >= dial.size) {
+                    currentIndexDial = 0 + (currentIndexDial - dial.size)
+                }
+            } else {
+                currentIndexDial = currentIndexDial - distance
+                if (currentIndexDial < 0) {
+                    currentIndexDial = 100 - currentIndexDial.absoluteValue
+                }
+            }
+            if (dial[currentIndexDial] == 0) {
+                zeroCounter++
+            }
+        }
+
+
+        return zeroCounter
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun cleanDistance(number: Int) {
+        val finalNumber: Int
+        if (number > 100) {
+            val divider = number % 100
+        }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+//    fun part2(input: List<String>): Int {
+//        return input.size
+//    }
+    val testInput = readInput("day1_test")
+
+//     Test if implementation meets criteria from the description, like:
+    check(part1(testInput) == 3)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("day1_input")
+//    check(part1(testInput) == 1)
+    val input = readInput("day1_input")
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
